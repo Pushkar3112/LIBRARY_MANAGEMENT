ef9b5dedd8a72bc86106e65753e8f7620069e10d
Pushkar3112 <pushkarladda@gmail.com>
2025-06-27 00:27:13 +0530

    Delete library_management_system directory

--- library_management_system/book.cpp
+++
@@ -1,9 +0,0 @@
-#include "book.h"
-
-book::book() {}
-
-book::book(string i, string t, string a, int c) : id(i), title(t), author(a), copies(c) {}
-
-bool book::available() { return copies > 0; }
-void book::decrease() { if (copies <= 0) throw runtime_error("book not available"); copies--; }
-void book::increase() { copies++; }
